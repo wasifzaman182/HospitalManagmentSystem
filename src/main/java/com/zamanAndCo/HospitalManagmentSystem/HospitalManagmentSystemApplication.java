5b4e8e6d5b33e23d7bf4d6ed7209ed96b8b78b6b
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2024-04-13 11:51:36 +0200

    project basic structure is in runnable state

---
+++ src/main/java/com/zamanAndCo/HospitalManagmentSystem/HospitalManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zamanAndCo.HospitalManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zamanAndCo/HospitalManagmentSystem/HospitalManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamanAndCo.HospitalManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
